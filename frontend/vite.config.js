cf0c23bca64af0b5840c86229762384106bd8d65
Jillian-ignited <Jillian@ignitedindustries.com>
2025-09-18 10:08:17 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,15 @@
-test
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: ".",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
+  }
+});
+
